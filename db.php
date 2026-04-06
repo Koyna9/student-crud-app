34ed72bc6edb9a665140d1721b88c231f8017a09
Koyna Gupta <koyna.gupta@mitwpu.edu.in>
2026-04-06 21:38:58 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
 $host = "localhost";        // or "127.0.0.1"
 $user = "root";              // your MySQL username
-$password = "bhavi";  // YOUR MySQL password (important!)
+$password = "Koyna";  // YOUR MySQL password (important!)
 $database = "studentdb";
 
 // Create connection
